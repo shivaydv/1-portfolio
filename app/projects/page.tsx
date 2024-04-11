742cc5bee050517b29a0c05fa23a86ae5a36408d
Shiva Yadav <shivaydv1256@gmail.com>
2024-04-11 22:53:26 +0530

    fix the error in deployment in project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,30 +1,12 @@
-"use client";
-import ProjectList from "@/components/ProjectList";
+import CategoryTabs from "@/components/CategoryTabs";
+
 import SectionHeading from "@/components/SectionHeading";
-import { categoryList } from "@/lib/data";
-import React, { useState } from "react";
 
 const page = () => {
-  const [name, setName] = useState("All");
   return (
     <div className=" text-center ">
       <SectionHeading heading={"All Projects"} className="lg:mb-2 border-0" />
-      <div
-        role="tablist"
-        className="tabs tabs-bordered w-fit flex flex-wrap justify-center max-lg:space-y-4 "
-      >
-        {categoryList.map((category, idx) => (
-          <button
-            role="tab"
-            key={idx}
-            onClick={() => setName(category)}
-            className={`tab ${category === name && "tab-active"}`}
-          >
-            {category}
-          </button>
-        ))}
-      </div>
-      <ProjectList category={name} />
+      <CategoryTabs />
     </div>
   );
 };
